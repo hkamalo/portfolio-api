522cf235f4c6552620f0bf7f9360b9bc06617ef8
Heranca <heranca.kamalo@gmail.com>
2021-05-31 10:25:41 +0200

    some change on email

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ process.on('beforeExit', () => {
 app.post('/contact', (req, res) => {
   const { company, firstname, lastname, email, message } = req.body;
 
+  console.log(company, firstname, lastname, email, message);
   // ------Check data are good-------- //
   const { error } = Joi.object({
     company: Joi.string().min(2).max(255).required(),
